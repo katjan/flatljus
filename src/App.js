18f8b7cb1da17b4ad01ba7fc7151f5238cbb27b8
Katja Röös <katja.roos@leanon.se>
2022-05-10 19:24:04 +0200

    omgsosorry

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          THE HACKED VERSION
+          THE BRIGHT SITE OF
         </p>
-        <p>We are still hacking</p>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
