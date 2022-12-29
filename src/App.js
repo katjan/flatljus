61decd7a8d526a4d5ea9b7d2923cbb748aefd93d
Katja Röös <katja.roos@leanon.se>
2022-12-29 14:44:15 +0100

    Add styling, snow and opening hours for restaurants

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import logo from './riksgransen.png';
-import './App.css';
+import logo from './riksgransen.png'
+import './App.css'
+import BusinessHours from './BusinessHours'
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
         <p>
           THE BRIGHT SITE OF
         </p>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
       </header>
+      <BusinessHours/>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
 
---
+++ src/BusinessHours.js
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import './bhstyles.css'; // Import the CSS file
+import Snowfall from 'react-snowfall';
+
+
+
+const BusinessHours = () => {
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const currentDay = new Date().getDay();
+  const today = daysOfWeek[currentDay];
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpanded = () => setExpanded(!expanded);
+
+  const places = [
+    {
+      name: 'Restaurang Lapplandia',
+      hours: {
+        Monday: 'Closed',
+        Tuesday: 'Closed',
+        Wednesday: 'Closed',
+        Thursday: 'Closed',
+        Friday: 'Closed',
+        Saturday: 'Closed',
+        Sunday: 'Closed',
+      },
+    },
+    {
+      name: 'Lobbybaren & Pelikanhyllan',
+      hours: {
+        Monday: 'Closed',
+        Tuesday: 'Closed',
+        Wednesday: 'Closed',
+        Thursday: 'Closed',
+        Friday: 'Closed',
+        Saturday: 'Closed',
+        Sunday: 'Closed',
+      },
+    },
+    {
+      name: 'Meteorologen Ski Lodge',
+      hours: {
+        Monday: 'Closed',
+        Tuesday: 'Closed',
+        Wednesday: 'Closed',
+        Thursday: 'Closed',
+        Friday: 'Closed',
+        Saturday: 'Closed',
+        Sunday: 'Closed',
+      },
+    },
+    {
+      name: 'Grönan',
+      hours: {
+        Monday: 'Closed',
+        Tuesday: 'Closed',
+        Wednesday: 'Closed',
+        Thursday: 'Closed',
+        Friday: 'Closed',
+        Saturday: 'Closed',
+        Sunday: 'Closed',
+      },
+    },
+    {
+      name: 'Lappis',
+      hours: {
+        Monday: 'Closed',
+        Tuesday: 'Closed',
+        Wednesday: 'Closed',
+        Thursday: 'Closed',
+        Friday: 'Closed',
+        Saturday: 'Closed',
+        Sunday: 'Closed',
+      },
+    },
+    {
+      name: 'Nordalsköket',
+      hours: {
+        Monday: 'Closed',
+        Tuesday: 'Closed',
+        Wednesday: 'Closed',
+        Thursday: 'Closed',
+        Friday: 'Closed',
+        Saturday: 'Closed',
+        Sunday: 'Closed',
+      },
+    },
+    {
+      name: 'Låktatjåkko Fjällstation',
+      hours: {
+        Monday: 'Closed',
+        Tuesday: 'Closed',
+        Wednesday: 'Closed',
+        Thursday: 'Closed',
+        Friday: 'Closed',
+        Saturday: 'Closed',
+        Sunday: 'Closed',
+      },
+    },
+    // ... other places
+  ];
+
+  return (
+    <div className="business-hours ski-theme">
+      <Snowfall />
+      <h2 className="business-hours__title">Business Hours</h2>
+      {places.map(place => (
+      <div className="usiness-hours__grid">
+        <div key={place.name} className="business-hours__place">
+          <h3
+            className="business-hours__place-name"
+            onClick={toggleExpanded}
+          >
+            {place.name}
+          </h3>
+          <ul className="business-hours__hours-list">
+            <li key={today} className="business-hours__hours-item">
+              {today}: {place.hours[today]}
+            </li>
+            {expanded &&
+              Object.entries(place.hours).map(([day, hours]) => (
+                <li key={day} className="business-hours__hours-item">
+                  {day}: {hours}
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+      ))}
+    </div>
+  );
+};
+
+export default BusinessHours;
