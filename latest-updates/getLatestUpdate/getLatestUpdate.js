f1e4b182c5e3c09104a86a09a94766c78c0e165c
Katja Röös <katja.roos@leanon.se>
2023-01-12 21:12:21 +0100

    Weather Funciton

--- latest-updates/getLatestUpdate/getLatestUpdate.js
+++ latest-updates/getLatestUpdate/getLatestUpdate.js
@@ -1,10 +1,27 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
+  const fetchWeatherContent = async () => {
+    try {
+      const response = await fetch(
+        'https://riksgransen.se/skidakning/snorapport-och-pistkarta/',
+        { mode: 'no-cors'})
+      console.log(2, response)
+
+      const html = await response.text()
+      console.log(3, html)
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(html, 'text/html')
+      const weatherDiv = doc.querySelector('.weather-content')
+      return weatherDiv
+    } catch (error) {
+      console.error(error)
+    }
+  }
   try {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ message: fetchWeatherContent() }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
